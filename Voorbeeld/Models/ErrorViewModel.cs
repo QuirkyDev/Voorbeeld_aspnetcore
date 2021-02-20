585eee48369f786f22717d65d74420bbb4c6a5ab
Joeri Verlooy <u0140090@thomasmore.be>
2021-02-20 04:29:45 +0100

    Add project files.

---
+++ Voorbeeld/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Voorbeeld.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
