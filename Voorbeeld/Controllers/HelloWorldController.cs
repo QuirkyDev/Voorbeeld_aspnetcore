a14b8f64a2a261636ed44807a7a113e83564aba3
Joeri <joeri.verlooy@gmail.com>
2021-02-20 05:31:57 +0100

    added HelloWorldController

---
+++ Voorbeeld/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Voorbeeld.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "Dit is de 'index' action method";
+        }
+
+        public string Welkom()
+        {
+            return "Dit is de 'welkom' action method";
+        }
+
+        public string Bestelling(int id)
+        {
+            return "Dit zijn de details van bestelling met id " + id;
+        }
+    }
+}
